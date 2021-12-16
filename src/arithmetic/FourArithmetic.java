e16e17ac8d0e9e3eb90b5b800a009a9b857a57f0
Sumin_Ann <annsumin2066@naver.com>
2021-12-16 22:01:19 +0900

    arithmetic_fourArithmetic

---
+++ src/arithmetic/FourArithmetic.java
@@ -0,0 +1,19 @@
+package arithmetic;
+
+import java.util.Scanner;
+
+public class FourArithmetic {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+		
+		scan.close();
+	}
+}
