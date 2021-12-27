0c1fb55bee7d220cd02e42a18e2adbf0eace57de
Sumin_Ann <annsumin2066@naver.com>
2021-12-27 22:06:45 +0900

    string_ACSII(알파벳 소문자, 대문자, 숫자0~9, 아스키 코드값 출력)

---
+++ src/string/AcsII.java
@@ -0,0 +1,13 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AcsII {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = br.read();
+		System.out.println(num);
+	}
+}
