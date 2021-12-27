891e5cd8716e5c5bdfca816b416ab5263f2ef598
Sumin_Ann <annsumin2066@naver.com>
2021-12-27 23:02:49 +0900

    string_alphabet(소문자 알파벳 처음 위치)

---
+++ src/string/Alphabet.java
@@ -0,0 +1,27 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Alphabet {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String st = br.readLine();
+		int[] alpha = new int[26];
+		for(int i=0; i<alpha.length;i++) {
+			alpha[i] = -1;
+		}
+		
+		for(int i=0; i<st.length(); i++) {
+			char ch = st.charAt(i);
+			if(alpha[ch-97] == -1) {
+				alpha[ch-97] = i;
+			}
+		}
+		
+		for(int i : alpha) {
+			System.out.print(i + " ");
+		}
+	}
+}
