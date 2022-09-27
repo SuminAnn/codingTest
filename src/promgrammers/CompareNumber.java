166a46ebb37aa47fa05d14e8b43a01cc607adf70
SuminAnn <annsumin2066@naver.com>
2022-09-27 16:10:39 +0900

    최대값과 최솟값(연습문제 Level 2)

--- src/promgrammers/CompareNumber.java
+++ src/promgrammers/CompareNumber.java
@@ -2,4 +2,20 @@ package promgrammers;
 
 public class CompareNumber {
 
+	public String solution(String s) {
+        String[] number = s.split(" ");
+        
+        return compare(number);
+    }
+    
+    public String compare(String[] number){
+        int[] nums = new int[number.length];
+        
+		for(int i=0; i<number.length; i++) {
+			nums[i] = Integer.parseInt(number[i]);
+		}
+        
+        Arrays.sort(nums);
+        return nums[0] + " " + nums[nums.length - 1];
+    }
 }
