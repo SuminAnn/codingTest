5f121e064e3cad16a1cbab5c3fc217ee503c03a5
SuminAnn <annsumin2066@naver.com>
2022-11-10 15:47:13 +0900

    카테고리 별 상품 개수 구하기(ORACLE)

---
+++ src/promgrammers/CountProductBYCategory.java
@@ -0,0 +1,13 @@
+package promgrammers;
+
+public class CountProductBYCategory {
+
+	public static void main(String[] args) {
+		SELECT
+		    SUBSTR(PRODUCT_CODE, 0, 2) AS CATEGORY,
+		    COUNT(*) AS PRODUCTS //상품 개수
+		FROM PRODUCT
+		GROUP BY SUBSTR(PRODUCT_CODE, 0, 2) //카테고리 코드별 그룹화
+		ORDER BY CATEGORY
+	}
+}
