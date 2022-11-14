c9d66f412044ebdf700bca8a22776ef08e081aca
SuminAnn <annsumin2066@naver.com>
2022-11-14 17:06:01 +0900

    진료과별 총 예약 횟수 출력하기(MS-sql)(count, group by)

---
+++ src/promgrammers/CountAppointment.java
@@ -0,0 +1,14 @@
+package promgrammers;
+
+public class CountAppointment {
+
+	public static void main(String[] args) {
+		SELECT
+		 MCDP_CD AS '진료과코드',
+		 count(*) AS '5월예약건수'
+		FROM APPOINTMENT
+		WHERE APNT_YMD LIKE '2022-05%' // 예약 월이 5월인
+		GROUP BY MCDP_CD // 진료과 코드를 기준으로 그룹화
+		ORDER BY count(MCDP_CD) ASC, MCDP_CD ASC
+	}
+}
