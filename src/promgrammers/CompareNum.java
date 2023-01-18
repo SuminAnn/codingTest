7306721e8fb9a6f5fd01d3dec744b1f924d9feda
SuminAnn <annsumin2066@naver.com>
2023-01-18 10:35:12 +0900

    코딩테스트입문(숫자 비교하기)

---
+++ src/promgrammers/CompareNum.java
@@ -0,0 +1,10 @@
+package promgrammers;
+
+public class CompareNum {
+
+	 public int solution(int num1, int num2) {
+	        int answer = 0;
+	        answer = num1==num2 ? 1 : -1; //두수가 같으면 1 다르면 -1을 return
+	        return answer;
+	    }
+}
