90c8f7856120a4591bd65cef3b5de7d31c501582
SuminAnn <annsumin2066@naver.com>
2023-01-19 11:12:12 +0900

    코딩테스트 입문(나머지 구하기)

---
+++ src/promgrammers/Remainder.java
@@ -0,0 +1,10 @@
+package promgrammers;
+
+public class Remainder {
+
+	 public int solution(int num1, int num2) {
+	        int answer = -1;
+	        answer = num1%num2;
+	        return answer;
+	    }
+}
