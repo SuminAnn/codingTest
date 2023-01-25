d81e2d3bb464675f4597c16abeb348c079cd5191
SuminAnn <annsumin2066@naver.com>
2023-01-25 10:38:48 +0900

    코딩테스트 입문(나이 출력)

---
+++ src/promgrammers/BornYear.java
@@ -0,0 +1,10 @@
+package promgrammers;
+
+public class BornYear {
+
+	public int solution(int age) {
+        int answer = 0;
+        answer = 2022-age+1; // 태어난 연도에 1살이기 때문에 +1
+        return answer;
+    }
+}
