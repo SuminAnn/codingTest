9277bc150fbc83d8596659bbfc394a5ef57f7ace
SuminAnn <annsumin2066@naver.com>
2023-01-25 17:44:15 +0900

    코딩테스트 입문(문자열 뒤집기)

---
+++ src/promgrammers/ReverseString.java
@@ -0,0 +1,11 @@
+package promgrammers;
+
+public class ReverseString {
+
+	 public String solution(String my_string) {
+	        String answer = "";
+	        StringBuffer sb = new StringBuffer(my_string);
+	        answer = sb.reverse().toString();
+	        return answer;
+	 }
+}
