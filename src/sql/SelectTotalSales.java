ca7bd3cca19e4f4e9254b3366b4e932fdf2c01e6
SuminAnn <annsumin2066@naver.com>
2023-03-22 10:43:17 +0900

    조건에 맞는 사용자와 총 거래금액 조회하기(InnerJoin, GroupBy, Having)_MySQL

---
+++ src/sql/SelectTotalSales.java
@@ -0,0 +1,18 @@
+package sql;
+
+public class SelectTotalSales {
+
+	public void solution() {
+		SELECT
+		    B.USER_ID,
+		    B.NICKNAME,
+		    SUM(A.PRICE) AS TOTAL_SALES
+		FROM USED_GOODS_BOARD A
+		INNER JOIN USED_GOODS_USER B
+		ON A.WRITER_ID = B.USER_ID
+		WHERE STATUS = 'DONE'
+		GROUP BY B.USER_ID
+		HAVING SUM(A.PRICE) >= 700000
+		ORDER BY TOTAL_SALES
+	}
+}
