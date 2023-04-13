04de51b0c9661d22bbf5344462b6f690e4e77bd9
SuminAnn <annsumin2066@naver.com>
2023-04-13 11:02:52 +0900

    조건에 맞는 도서 리스트 출력하기(DATE_FORMAT, YEAR)_MySQL

---
+++ src/sql/SelectBookList.java
@@ -0,0 +1,14 @@
+package sql;
+
+public class SelectBookList {
+
+	public void solution() {
+		SELECT
+		    BOOK_ID,
+		    DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
+		FROM BOOK
+		WHERE YEAR(PUBLISHED_DATE) = 2021
+		AND CATEGORY = '인문'
+		ORDER BY PUBLISHED_DATE
+	}
+}
