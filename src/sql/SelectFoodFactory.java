fc1836bf3697ed0a1185139377d02ae9e3e3b4ba
SuminAnn <annsumin2066@naver.com>
2023-04-24 10:34:45 +0900

    강원도에 위치한 생산공장 목록 출력하기(SUBSTRING)_MySQL

---
+++ src/sql/SelectFoodFactory.java
@@ -0,0 +1,14 @@
+package sql;
+
+public class SelectFoodFactory {
+
+	public void solution() {
+		SELECT
+		    FACTORY_ID	
+		    ,FACTORY_NAME	
+		    ,ADDRESS
+		FROM FOOD_FACTORY
+		WHERE SUBSTRING(ADDRESS, 1,3) = '강원도'
+		ORDER BY FACTORY_ID
+	}
+}
