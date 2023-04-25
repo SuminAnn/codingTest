b9e2bc61c85c67f17acabb3afa13d7801b0c6d24
SuminAnn <annsumin2066@naver.com>
2023-04-25 11:36:35 +0900

    없는 숫자 더하기(월간 코드 챌린지 시즌3 Level1)

---
+++ src/promgrammers/AbsentNumbers.java
@@ -0,0 +1,20 @@
+package promgrammers;
+
+public class AbsentNumbers {
+
+	public int solution(int[] numbers) {
+        int answer = 0;
+        int[] cnt = new int[10];
+        
+        for(int i=0; i<numbers.length; i++){
+            cnt[numbers[i]]++;
+        }
+        
+        for(int i=0; i<cnt.length; i++){
+            if(cnt[i] == 0){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
