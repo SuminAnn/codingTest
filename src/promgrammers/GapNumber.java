f859b6082371d1cb78868a342b000bce9dca357e
SuminAnn <annsumin2066@naver.com>
2023-05-19 11:15:02 +0900

    x만큼 간격이 있는 n개의 숫자(Level 1)

---
+++ src/promgrammers/GapNumber.java
@@ -0,0 +1,15 @@
+package promgrammers;
+
+public class GapNumber {
+
+	public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long num = x;
+        
+        for(int i=0; i<n; i++){
+            answer[i] = num;
+            num += x;
+        }
+        return answer;
+    }
+}
