28eff52ccd8b3ba573b3fc3422dc47f19e938aa5
SuminAnn <annsumin2066@naver.com>
2023-05-23 10:34:56 +0900

    괄호 회전하기(월간 코드챌린지 시즌2 Level2)

---
+++ src/promgrammers/BracketRotation.java
@@ -0,0 +1,29 @@
+package promgrammers;
+
+public class BracketRotation {
+	public int solution(String s) {
+        int answer = 0;
+        for(int i=0; i<s.length(); i++){
+            String str = s.substring(i, s.length()) + s.substring(0,i);
+            Stack<Character> stack = new Stack<>();
+            for(int j=0 ;j<str.length(); j++){
+                char c = str.charAt(j);
+                if(stack.isEmpty()){
+                    stack.push(c);
+                }else if(c == ')' && stack.peek() == '('){
+                    stack.pop();
+                }else if(c == ']' && stack.peek() == '['){
+                    stack.pop();
+                }else if(c == '}' && stack.peek() == '{'){
+                    stack.pop();
+                }else{
+                    stack.push(c);
+                }
+            }
+            if(stack.isEmpty()){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
