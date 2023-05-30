3db645f2849d499e0cf85b9506495c95ee8dbfe8
SuminAnn <annsumin2066@naver.com>
2023-05-30 10:18:21 +0900

    짝수와 홀수(연습문제 Level1)

---
+++ src/promgrammers/OddAndEven.java
@@ -0,0 +1,13 @@
+package promgrammers;
+
+public class OddAndEven {
+
+	 public String solution(int num) {
+	        String answer = "";
+	        if(num%2 == 0){
+	            return "Even";
+	        }else{
+	            return "Odd";
+	        }
+	    }
+}
