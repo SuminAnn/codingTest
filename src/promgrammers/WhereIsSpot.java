b7dbb51388a975bf012c0d11b4f815e4e3ea136b
SuminAnn <annsumin2066@naver.com>
2023-06-28 14:39:42 +0900

    점의 위치 구하기(코딩테스트 입문 Level0)

---
+++ src/promgrammers/WhereIsSpot.java
@@ -0,0 +1,16 @@
+package promgrammers;
+
+public class WhereIsSpot {
+
+	public int solution(int[] dot) {
+        if(dot[0] > 0 && dot[1] > 0){
+            return 1;
+        }else if(dot[0] < 0 && dot[1] > 0){
+            return 2;
+        }else if(dot[0] < 0 && dot[1] < 0){
+            return 3;
+        }else{
+            return 4;
+        }
+    }
+}
