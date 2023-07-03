24b6b2c4a8e50ef0d6ce7af72dbf1ff8061c402b
SuminAnn <annsumin2066@naver.com>
2023-07-03 11:19:19 +0900

    예상 대진표(2017 팁스타운 Level2)

---
+++ src/promgrammers/ListOfMatch.java
@@ -0,0 +1,23 @@
+package promgrammers;
+
+public class ListOfMatch {
+
+	public int solution(int n, int a, int b)
+    {
+         int answer = 1;
+
+        while(true){
+
+            a = (a+1)/2;
+            b = (b+1)/2;
+
+            if(a == b)
+                break;
+
+            answer++;
+
+        }
+
+        return answer;
+    }
+}
