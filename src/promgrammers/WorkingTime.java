610a5fa08f2e4480f0ba1fa409262629cf4b0457
SuminAnn <annsumin2066@naver.com>
2023-07-26 13:22:36 +0900

    근무시간(softeer Level1)

---
+++ src/promgrammers/WorkingTime.java
@@ -0,0 +1,30 @@
+package promgrammers;
+
+import java.util.*;
+import java.io.*;
+public class WorkingTime {
+
+	 public static void main(String args[])throws IOException
+	    {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int tot = 0;
+	        
+	        System.out.print(tot);
+
+	    }
+	 public static int cal(int tot, BufferedReader br)throws IOException {
+		 StringTokenizer st;
+		 for(int i=0; i<5; i++){
+			 st = new StringTokenizer(br.readLine(), " ");
+	         String go = st.nextToken();
+	         String leave = st.nextToken();
+	         int goH = Integer.parseInt(go.substring(0,2));
+	         int goM = Integer.parseInt(go.substring(3,5));
+	         int lvH = Integer.parseInt(leave.substring(0,2));
+	         int lvM = Integer.parseInt(leave.substring(3,5));
+	         tot += (lvH-goH)*60 + (lvM-goM);
+	    }
+		 return tot;
+	 }
+}
