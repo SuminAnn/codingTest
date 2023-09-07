de5e1c193b71159c621b796e72bd185c395364f8
SuminAnn <annsumin2066@naver.com>
2023-09-07 11:20:44 +0900

    2016년(연습문제 Level1)

---
+++ src/promgrammers/Calendar.java
@@ -0,0 +1,19 @@
+package promgrammers;
+
+public class Calendar {
+
+	public String solution(int a, int b) {
+        String answer = "";
+        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int sum = 0;
+        
+        for(int i=0; i<a-1; i++){
+            sum += date[i];
+        }
+        sum += (b-1);
+        
+        answer = days[sum%7];
+        return answer;
+    }
+}
