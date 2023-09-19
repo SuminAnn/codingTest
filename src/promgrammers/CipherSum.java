61f130a0ec4f62bc9b7d18a1c981f9d579a1311e
SuminAnn <annsumin2066@naver.com>
2023-09-19 15:52:25 +0900

    자릿수 더하기(연습문제 Level1)

---
+++ src/promgrammers/CipherSum.java
@@ -0,0 +1,15 @@
+package promgrammers;
+
+public class CipherSum {
+
+	public int solution(int n) {
+        int answer = 0;
+
+        while(n>0){
+            answer += n%10;
+            n = n/10;
+        }
+
+        return answer;
+    }
+}
